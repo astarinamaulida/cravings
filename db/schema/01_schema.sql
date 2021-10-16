824b52262aa89bd748fc671e24a77a355fec871f
tonyloba <tonyloba@gmail.com>
2021-10-16 00:52:41 +0000

    updated scema files to create db and tables

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,9 +1,13 @@
 -- Drop and recreate Users table (Example)
 DROP TABLE IF EXISTS customers CASCADE;
-DROP TABLE IF EXISTS customers CASCADE;
-DROP TABLE IF EXISTS customers CASCADE;
-DROP TABLE IF EXISTS customers CASCADE;
-
+DROP TABLE IF EXISTS carts CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS pickups CASCADE;
+DROP TABLE IF EXISTS payments CASCADE;
+DROP TABLE IF EXISTS menus CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE DATABASE carvingsdb;
+\c carvingsdb;
 
 
 
@@ -17,15 +21,16 @@ CREATE TABLE carts (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
   menu_id INTEGER REFERENCES menus(id) ON DELETE CASCADE,
-  placed_order DATE
+  placed_order TIMESTAMP,
+  isOrdered BOOLEAN
 );
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
-  foods_in_cart VARCHAR(255) NOT NULL,
-  order_received DATE,
-  order_processed DATE,
-  order_sent DATE,
+  menu_id INTEGER REFERENCES menus(id) ON DELETE CASCADE,
+  order_received TIMESTAMP,
+  order_processed TIMESTAMP,
+  order_sent TIMESTAMP,
 
 );
 CREATE TABLE pickups (
@@ -35,3 +40,29 @@ CREATE TABLE pickups (
   customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
   is_paid BOOLEAN
 );
+CREATE TABLE payments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  payment_type VARCHAR(255) NOT NULL,
+  payment_date TIMESTAMP,
+  amount INTEGER NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+);
+CREATE TABLE menus (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+
+);
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  isProcessed BOOLEAN,
+  isReady BOOLEAN,
+  price INTEGER NOT NULL
+);
+
+\dt
+
+-- to run : \i  db/schema/01_schema.sql
+
