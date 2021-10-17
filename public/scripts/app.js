d4cc5ca40018acd5b02d73d1d2d88e0034d2eda8
hlai16 <gloriajoanlai@gmail.com>
2021-10-16 21:01:32 -0400

    commit just in case

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,1 @@
 // Client facing scripts here
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/order_signup", (req, res) => {
-    console.log(req.body);
-    res.render('order_signup.ejs');
-  });
