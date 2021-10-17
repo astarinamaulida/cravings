6e98b0ddc61fffe18055e999eaff4ef55ca2b981
hlai16 <gloriajoanlai@gmail.com>
2021-10-17 11:34:20 -0400

    example of how to add real thumbnail to url and edit in header.scss

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,7 +1,7 @@
 INSERT INTO
   menu_items (name, description, unit_price, thumbnail_url,item_category)
 VALUES
-('Donut','Boston glazed',100,'https://photos.cravings.com','category'),
+('Donut','Boston glazed',100,'https://github.com/astarinamaulida/cravings/blob/master/doc/cake-pic1.jpeg?raw=true','category'),
 ('Cupcake','Creamy',200,'https://photos.cravings.com','category'),
 ('Muffin','Powdered',400,'https://photos.cravings.com','category');
 
