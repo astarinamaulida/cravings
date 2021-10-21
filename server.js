45c76a33bd66fd1172820cf83468308782dea4ba
Astarina Maulida <astarina.maulida@gmail.com>
2021-10-21 16:14:18 -0400

    Fix conflicts

--- server.js
+++ server.js
@@ -53,14 +53,10 @@ const order_itemsRoutes = require("./routes/order_items");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(db));
-// app.use("/orders", ordersRoutes(db));
+// app.use("/api/orders", ordersRoutes(db));
 app.use("/menu_items", menu_itemsRoutes(db));
 app.use("/order_items", order_itemsRoutes(db));
-
-// const smsRoutes = require("./routes/sms");
-// app.use("/sms", smsRoutes(client, db));
-// app.use("/sms/sms-response", smsRoutes());
-
+// Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file! Separate them into separate routes files (see above).
@@ -82,17 +78,17 @@ app.get("/order_menu", (req, res) => {    ///need to change (for order_items) th
   res.render("order_menu", templateVars);
 })
 
-app.get("/order_index", (req, res) => {
+app.get("/cart", (req, res) => {
   const user = req.session.user_id;
   const templateVars = { user };
   if (!user) {
     return res.redirect('/order_signup');
   }
-  res.render("order_index", templateVars);
+  res.render("cart", templateVars);
 });
-app.get("/cart", (req, res) => {
-  res.render("cart");
-})
+// app.get("/cart", (req, res) => {
+//   res.render("cart");
+// })
 
 // POST ADD TO CART BUTTON TO CHECKOUT CART (USING NEW PATH ORDER_ITEMS SO IT WON'T DISTURB ORDER_INDEX)
 
@@ -121,14 +117,18 @@ app.get("/order_add_cart", (req, res) => {
 })
 
 /// route for order_items:
-app.get("/order_items", (req, res) => {
+app.get("/api/order_items", (req, res) => {
+  // req.session.order_items = order_items;   ???? is expression correct ?
+});
+
+
 
 // Twilio API
 
 
-const accountSid = 'AC7ea74d71de54f739088cd9dd81557de4'; //PUT YOUR SID in ""
-const authToken = '0fd2a881fdaaa7c2838ab1dcb57ce148'; //PUT YOUR Token in ""
-const client = require('twilio')(accountSid, authToken);
+//const accountSid = ''; //PUT YOUR SID in ""
+//const authToken = ''; //PUT YOUR Token in ""
+//const client = require('twilio')(accountSid, authToken);
 
 
 // Checkout page
@@ -137,8 +137,6 @@ app.get("/checkout", (req, res) => {
 })
 
 app.post("/checkout", (req, res) => {
-  console.log(req.params)
-  console.log(req.body)
 
   // Send SMS to restaurant through Twilio
   client.messages
@@ -156,16 +154,28 @@ app.post("/checkout", (req, res) => {
   .create({
       body: 'Thank you for ordering from Cravings. Your order will be ready in 10 min.',
       from: '+12494881210',  // from TWilio phone
-      to:  `+${req.body.phone}`//`+${document.getElementById('phone').value}`   // put your phone to test it
+      to:  '+14379228484'//`+${document.getElementById('phone').value}`   // put your phone to test it
   })
-  .then(message => console.log(message.sid))
-  .catch(console.error)
+  .then(message => {
+    console.log(message.sid)
+    const phone = req.body.phone;
+    console.log('phone', phone);
+  })
+  .catch(err => {
+    console.log('error', error);
+    res.redirect('/');
+  })
   .done();
 
   req.session = null;
   res.redirect("/");
 })
-});
+
+
+
+
+
+
 
 
 app.listen(PORT, () => {
