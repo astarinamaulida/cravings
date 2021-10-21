2b6a7234ae11fb6316583b01776c786a8b9eb7a0
hlai16 <gloriajoanlai@gmail.com>
2021-10-20 22:56:56 -0400

    removing remove item button

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,14 +1,12 @@
 
 
 $(() => {
-        
-    const populateCart = () => {
-        let total = 0;
-        let products = JSON.parse(localStorage.getItem('products'));
-        console.log('products', products);
-        for (const obj of products) {
-            console.log('obj', obj);
-                let container = `
+  const populateCart = () => {
+    let total = 0;
+    let products = JSON.parse(localStorage.getItem('products'));
+    for (const obj of products) {
+      console.log('obj', obj);
+      let container = `
         <div class="card mb-3" style="max-width: 540px;" id="${obj.productId}">
         <div class="row g-0">
           <div class="col-md-4 cart-imgs">
@@ -20,22 +18,56 @@ $(() => {
               <p class="card-text">${obj.productDescription}</p>
               <p class="card-text">Unit price: $${obj.productUnitPrice}</p>
             </div>
-            <div id="remove-item" onCick="removeItem($('#${obj.productId}))">
-              <button>Remove Item</button>
-            </div>
           </div>
         </div>
       </div>
                 `
-                $("#order-items").append(container);
-                total = total + parseFloat(obj.productUnitPrice)
-                $(".totals-value").html(total);
-            }
-        
+      $("#order-items").append(container);
+      total = total + parseFloat(obj.productUnitPrice)
+      $(".totals-value").html(total);
     }
 
+  }
+
+  // const removeItem = function (id) {
+  //   console.log('id', id);
+  //   let products = [];
+  //   if (localStorage.getItem('products')) {
+  //     products = JSON.parse(localStorage.getItem('products'));
+  //   }
+  //   for (const obj of products) {
+  //     if (obj.productId === id) {
+  //       $(`#${id}`).remove();
+  //     }
+      
+  //     let total = $(".totals-value").val();
+  //     console.log('total', total);
+  //     total = total - parseFloat(obj.productUnitPrice)
+  //     $(".totals-value").html(total);
+  //   }
+  //   localStorage.setItem('products', JSON.stringify(products));
+  // }
+
+  // const removeItem = function () {
+  //   $("#click-to-remove").click(function (event) {
+  //     event.preventDefault();
+  //     console.log('clicked');
+  //     const itemId = $(event.target.value);
+  //     console.log('itemId', itemId);
+  //     let products = JSON.parse(localStorage.getItem('products'));
+  //     $(this).parent().parent().parent().parent().remove();
+
+  //     total = total - parseFloat(cart[itemId].productUnitPrice)
+
+  //     $(".totals-value").html(total)
+  //     delete products[itemId]
 
 
-    populateCart();
+  //     localStorage.setItem("products", JSON.stringify(products))
+  //   })
+  // }
 
+
+  populateCart();
+  removeItem();
 })
