a89b1725a7364c39d86d7598041436ad9c9db60a
hlai16 <gloriajoanlai@gmail.com>
2021-10-21 18:39:36 -0400

    Order message rendering successfully

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,18 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $("#submit").on('click', function(event) {
+  $("#submit").on('click', function (event) {
     event.preventDefault();
-      let name = $("#name").val();
-     let phone = $("#phone").val();
-     
-        $.ajax({
-            method: 'POST',
-            url: "/checkout",
-            data: {
-              name,
-              phone
-            }
-        })
-})
+    let name = $("#name").val();
+    let phone = $("#phone").val();
+
+    $.ajax({
+      method: 'POST',
+      url: "/checkout",
+      data: {
+        name,
+        phone
+      }
+    })
+  })
+  $("#submit").on('click', function (event) {
+    event.preventDefault();
+    let name = $("#name").val();
+    $('#orderingMsg').empty();
+    // need to have .empty(), else message will keep on printing out non-stop whenever client press submit button.
+    $('#orderingMsg').append(`Hey ${name}, thank you for ordering from Cravings. Your order will be ready in 10 min.😉`);
+  })
 
 });
